64211cebecb349e64db64936659dcdbdd0350921
vadim_budarin <budarin-vv@yandex.ru>
2021-12-15 02:11:03 +0300

    fix: code

--- src/simple/renderApp.mjs
+++ src/simple/renderApp.mjs
@@ -5,13 +5,15 @@ const script = fs.readFileSync(path.resolve('src/simple/script.js'), { encoding:
 
 export const renderApp = (ctx) => {
   ctx.body = `<html lang="ru" data-type="SSR">
-  <head>
-      <meta charset="UTF-8"/>
-  </head>
-  <body>
-    <input type="file" id="fileLoader"></input>
-    <script>${script}</script>
-  </body>
+<head>
+    <meta charset="UTF-8"/>
+</head>
+<body>
+<input type="file" id="fileLoader"></input>
+<script>
+${script}
+</script>
+</body>
 </html>
   `;
 };
