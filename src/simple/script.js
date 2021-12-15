8d152751e490e1b74d3b5233e9e650fa266eb944
vadim_budarin <budarin-vv@yandex.ru>
2021-12-15 04:37:12 +0300

    fix: code

--- src/simple/script.js
+++ src/simple/script.js
@@ -1,9 +1,16 @@
-const uploadFile = (file) => {
+function send(xhr, body) {
+    return new Promise((resolve, reject) => {
+        xhr.onload = () => resolve(xhr);
+        xhr.onerror = reject;
+        xhr.send(body);
+    });
+}
+
+const uploadFile = async (file) => {
     if (file) {
         const formData = new FormData();
         const xhr = new XMLHttpRequest();
 
-        xhr.withCredentials = true;
         xhr.open('POST', '/upload', true);
 
         xhr.setRequestHeader('Accept', 'application/json');
@@ -17,31 +24,37 @@ const uploadFile = (file) => {
         };
 
         xhr.upload.ontimeout = function () {
-            console.log("timeout error");
+            console.log('timeout error');
         };
 
         xhr.upload.onload = function () {
-            console.log("Загружено: " + xhr.status + "  " + xhr.response + " " + xhr.readyState);
+            console.log('Загружено: ' + xhr.status + '  ' + xhr.response + ' ' + xhr.readyState);
         };
 
         xhr.upload.onerror = function (ev) {
             if (this.status == 200) {
                 console.log('success');
             } else {
-                console.log("error " + this.status);
+                console.log('error ' + this.status);
             }
         };
 
         console.log('start sending form');
 
-        xhr.send(file);
+        await send(xhr, file);
+
+        if (xhr.status === 201) {
+            console.log(JSON.parse(xhr.response));
+        } else {
+            console.log('error', xhr.status);
+        }
     }
-}
+};
 
 const onFileChange = (event) => {
     if (event.target.files?.length) {
         const file = event.target.files[0];
-        uploadFile(file);
+        void uploadFile(file);
     }
 };
 
--- src/simple/uploadFile.mjs
+++ src/simple/uploadFile.mjs
@@ -1,36 +1,36 @@
-import { createWriteStream } from 'fs'
-import { inspect } from "util";
+import { createWriteStream } from 'fs';
+import { inspect } from 'util';
 
 const { log } = console;
 
 export const uploadFile = async (ctx) => {
-  const p = new Promise((resolve, reject) => {
-    const fileName = decodeURIComponent(ctx.get('X-filename'));
-    const writableStream = createWriteStream(fileName);
+    const p = new Promise((resolve, reject) => {
+        const fileName = decodeURIComponent(ctx.get('X-filename'));
+        const writableStream = createWriteStream(fileName);
 
-    writableStream.on('error', (error) => {
-      log('error wririnf file', inspect(error))
-      reject(error);
+        writableStream.on('error', (error) => {
+            log('error wririnf file', inspect(error));
+            reject(error);
+        });
+
+        writableStream.on('finish', () => {
+            log('finished writing file');
+            resolve();
+        });
+
+        ctx.req.pipe(writableStream);
     });
 
-    writableStream.on('finish', () => {
-      log('finished writing file')
-      resolve();
-    });
+    await p
+        .then(() => {
+            ctx.set('Connection', 'close');
+            ctx.status = 201;
+            ctx.body = { result: 'Ok' };
+        })
+        .catch((error) => {
+            log('error', inspect(error));
 
-    ctx.req.pipe(writableStream);
-  });
-
-  await p
-    .then(() => {
-      ctx.set("Connection", "close");
-      ctx.status = 200;
-      ctx.body = { result: "Ok" };
-    })
-    .catch((error) => {
-      log('error', inspect(error));
-
-      ctx.status = 500;
-      ctx.body = error.message;
-    });
+            ctx.status = 500;
+            ctx.body = error.message;
+        });
 };
